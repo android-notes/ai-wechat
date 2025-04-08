f3c54dbe34220e9f438506de7cd97be4bcab0c9b
WJ <hellowanjian@qq.com>
2025-04-08 20:10:18 +0800

    元素捕获完成

--- code/e072f99fb279bd287f6084532972a28.py
+++ code/e072f99fb279bd287f6084532972a28.py
@@ -12,10 +12,12 @@ def run(args):
 
     if args:
         pass
-    ___l(1, "打开网址<s>hhrpa.com</s>，将网页对象保存到<v>web_page</v>", {'debug':False})
-    web_page = open_web_page("hhrpa.com", "Google Chrome")
-    ___l(2, "点击网页<v>web_page</v>中的元素<s>使用帮助</s><img src=\"app://res\\elements\\ddc28d00-9906-4273-8b99-09eb2cfcafa1.png?1744017001487\" element=\"\"/>", {'debug':False})
-    click_web_element(web_page, ElementDescriptor('ddc28d00-9906-4273-8b99-09eb2cfcafa1', "使用帮助"), {"clickOption":"鼠标点击","hoverOption":"鼠标悬停","clickType":"单击","mouseKey":"左键","modifierKey":"无","delay":"0","wait":"3","position":"随机","inputType":"模拟人工输入","eng":False,"append":False,"tab":False,"enter":False,"click":True,"focusTimeout":"1"})
+    ___l(1, "获取元素<s>搜索输入框</s><img src=\"app://res\\elements\\edf58636-3b8a-4587-9509-953d807f8394.png\" element=\"\"/>所属的窗口并保存到<v>微信窗口</v>", {'debug':False})
+    微信窗口 = get_window_instance_by_element(ElementDescriptor('edf58636-3b8a-4587-9509-953d807f8394', "搜索输入框"))
+    ___l(2, "设置窗口<v>微信窗口</v>的状态为<s>最大化</s>", {'debug':False})
+    set_window_status(微信窗口, '最大化')
+    ___l(3, "在输入框<s>搜索输入框</s><img src=\"app://res\\elements\\edf58636-3b8a-4587-9509-953d807f8394.png\" element=\"\"/>输入<s>文件传输助手</s>", {'debug':False})
+    input_win_element(ElementDescriptor('edf58636-3b8a-4587-9509-953d807f8394', "搜索输入框"), "文件传输助手", {"clickOption":"鼠标点击","hoverOption":"鼠标悬停","clickType":"单击","mouseKey":"左键","modifierKey":"无","delay":"2","wait":"10","position":"随机","inputType":"模拟人工输入","eng":True,"append":False,"tab":False,"enter":True,"click":True,"focusTimeout":"1"})
 
 @call_flow_wrapper("主流程")
 def main(args=None):
